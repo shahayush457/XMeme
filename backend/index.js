92c465487e2a338ec84fcb610b48a6284d6bfb55
shahayush457 <shahayush457@gmail.com>
2021-02-06 12:24:15 +0530

    Irrelevant files removed

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const db = require("./db");
 const memes = require("./routes/memes");
 
@@ -10,6 +11,8 @@ dotenv.config();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
+
+app.use(cors);
 
 // Use Routes
 app.use("/", memes);
