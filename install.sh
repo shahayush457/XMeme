f472b728fffe60791fd4cc7f8be2bf5e0315c940
shahayush457 <shahayush457@gmail.com>
2021-02-08 01:09:57 +0530

    Server installation scripts and api tests added

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Any installation related commands
+sudo apt update -y
+
+sudo apt install -y mongodb
+
+sudo apt install -y nodejs
+
+sudo apt install -y npm
+
+# Any configuration related commands
+
+#sudo ufw allow 8081
